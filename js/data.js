526835fed7b73da55ccce840cf2faf4e6640f1c3
Lindsay M <lminami23@punahou.edu>
2018-08-11 11:01:18 -1000

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,62 @@
+var blogs = [
+	{
+		title: 'blog post 1',
+		date: 'Saturday, August 11, 2018',
+		post: 'today is saturday. this is a blog post... ok all done'
+	}
+	{
+		title: 'blog post 0',
+		date: 'Friday, August 10, 2018',
+		post: 'today is not friday. this didnt exist yet... this is a blog post... ok all done'
+	}
+	{
+		title: 'blog post -1',
+		date: 'Thursday, August 9, 2018',
+		post: 'today is not thursday. this didnt exist yet... this is a blog post... ok all done'
+	}
+	{
+		title: 'blog post -2',
+		date: 'Wednesday, August 8, 2018',
+		post: 'today is not wednesday. this didnt exist yet... this is a blog post... ok all done'
+	}
+	{
+		title: 'blog post -3',
+		date: 'Tuesday, August 7, 2018',
+		post: 'today is not tuesday. this didnt exist yet... this is a blog post... ok all done'
+	}
+	{
+		title: 'blog post -4',
+		date: 'Monday, August 6, 2018',
+		post: 'today is not monday. this didnt exist yet... this is a blog post... ok all done'
+	}
+	{
+		title: 'blog post -5',
+		date: 'Sunday, August 5, 2018',
+		post: 'today is not sunday. this didnt exist yet... this is a blog post... ok all done'
+	}
+	{
+		title: 'blog post -6',
+		date: 'Saturday, August 4, 2018',
+		post: 'today is saturday (but "today" was a week ago.) this didnt exist yet... this is a blog post... ok all done'
+	}
+	{
+		title: 'blog post -7',
+		date: 'Friday, August 3, 2018',
+		post: 'today is not friday, this isnt even the same week. this didnt exist yet... this is a blog post... ok all done'
+	}
+	{
+		title: 'blog post -8',
+		date: 'Thursday, August 2, 2018',
+		post: 'today is not thursday, this isnt even the same week. this didnt exist yet... this is a blog post... ok all done'
+	}
+	{
+		title: 'blog post -9',
+		date: 'Wednesday, August 1, 2018',
+		post: 'today is not wednesday, this isnt even the same week. this didnt exist yet... this is a blog post... ok all done'
+	}
+	{
+		title: 'blog post -10',
+		date: 'Tuesday, July 31, 2018',
+		post: 'today is not tuesday and ITS NOT JULY, and this isnt even the same week. this didnt exist yet... this is a blog post... ok all done'
+	}
+]
